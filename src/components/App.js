221221bac94b9cc3c341565cbace2908c121d03e
johnny <johnnyflores@live.ca>
2018-05-04 18:13:34 -0400

    Insert header, content and footer components

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
-import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Components
 import Header from './Global/Header';
 import Content from './Global/Content';
 import Footer from './Global/Footer';
-
-// Data
-import items from '../data/menu';
 
 class App extends Component {
   static propTypes = {
@@ -20,23 +15,11 @@ class App extends Component {
 
   render() {
     const { children } = this.props;
-
     return (
       <div className="App">
-        <MuiThemeProvider>
-          <AppBar
-            title="App ReactJS"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-          />
-        </MuiThemeProvider>
-        <Header
-          title="ReactJS"
-          items={items}
-        />
+        <Header />
         <Content body={children} />
         <Footer />
-
-
       </div>
     );
   }
