4f479edbf856b1a7c086a20ffefdd18ae2647179
johnny <johnnyflores@live.ca>
2018-05-04 18:21:14 -0400

    Insert slider component in home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+// Components
+import Slider from '../../components/Slider';
+
 class Home extends Component {
   static propTypes = {
     isMobile: PropTypes.bool
@@ -10,11 +13,10 @@ class Home extends Component {
 
   render() {
     const { isMobile } = this.props;
-
     return (
       <div className="Home">
+        <Slider />
         <h1>Home Page</h1>
-
         <p>
           {isMobile ? 'Mobile device' : 'Desktop device'}
         </p>
